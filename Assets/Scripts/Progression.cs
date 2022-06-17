52897da2d0f0206de67e18490c84260b31e087c6
Arsal Fadilah <88387951+arsalf@users.noreply.github.com>
2022-06-17 10:34:28 +0700

    Add Mekanik and Ui GamePlay

--- Assets/Scripts/Progression.cs
+++ Assets/Scripts/Progression.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 [System.Serializable]
 public class Progression : MonoBehaviour
 {
-    public string instruction;
+    public string instructionName;
     public bool isDone;
+    
+    public void pressKeyInstruction(string key)
+    {
+        if(Input.GetKeyDown(key))
+        {
+            isDone = true;
+        }
+    }
 }
